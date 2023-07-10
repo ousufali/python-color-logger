37418c5a1095a3ab1aa4165b0e89fb1aa5e5e117
ousufali <yousaf.syed5@gmail.com>
2023-07-11 00:53:05 +0500

    add ingress endpoint '/logger'

---
+++ routes/logger.py
@@ -0,0 +1,33 @@
+from flask import Blueprint
+from utils.page import html_content, home_html_content
+from utils.custom_logger import getLogger
+logger = getLogger()
+logger_blueprint = Blueprint('logger', __name__, url_prefix='/logger')
+
+@logger_blueprint.route('/')
+def index():
+    return home_html_content
+
+
+@logger_blueprint.route('/debug', methods=['GET', 'POST'])
+def debug():
+    logger.debug('This is a debug-level message')
+
+    return html_content.format(color="white")
+
+@logger_blueprint.route('/info', methods=['GET', 'POST'])
+def info():
+    logger.info('This is an info-level message')
+
+    return html_content.format(color="blue")
+
+@logger_blueprint.route('/warning', methods=['GET', 'POST'])
+def warning():
+    logger.warning('This is a warning-level message,This is a warning-level message,This is a warning-level message,This is a warning-level message,This is a warning-level message')
+
+    return html_content.format(color="yellow")
+
+@logger_blueprint.route('/error', methods=['GET', 'POST'])
+def error():
+    logger.error('This is an error-level message')
+    return html_content.format(color="red")
---
+++ utils/page.py
@@ -0,0 +1,19 @@
+html_content = '''
+<body     style="background-color: gray;">
+    <div style="text-align: center;">
+        <h1 style="color: {color};"> Colored Logger Application ( info/warning/debug/error )</h1>
+        <p style="color: {color};">Hello, how you doing I'm Yousuf the owner of this fucking collored logger application.
+        <h2> You know you are accesing it with ingress endpoint.</h2>
+        <p> Use /logger enpoint to access logging messages.</p>
+        </p>
+    </div>
+</body>
+'''
+home_html_content = '''
+<body
+    style="background-color: gray;">
+    <div style="text-align: center;">
+        '<p style="font-size: 180px; color: pink;">Chala Ja bsdk</p>'
+    </div>
+</body>
+'''
